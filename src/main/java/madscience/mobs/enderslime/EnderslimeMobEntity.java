6dafdc1285084f2cbdee645bba34187681143e2a
Maxwolf Goodliffe <devs@madsciencemod.com>
2014-02-22 05:38:24 -0800

    Merges AI of Enderman and Slime together for Enderslime and made him drop Enderslime items on death. Added new items to localization files.

--- src/main/java/madscience/mobs/enderslime/EnderslimeMobEntity.java
+++ src/main/java/madscience/mobs/enderslime/EnderslimeMobEntity.java
@@ -1,18 +1,33 @@
 package madscience.mobs.enderslime;
 
+import java.util.Random;
+
 import madscience.MadComponents;
+import madscience.MadConfig;
+import madscience.MadSounds;
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.monster.EntityGhast;
 import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntityDamageSourceIndirect;
 import net.minecraft.util.MathHelper;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 
 public class EnderslimeMobEntity extends EntityLiving implements IMob
 {
@@ -22,6 +37,13 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
     public float squishAmount;
 
     public float squishFactor;
+
+    private Random rand = new Random();
+
+    /** Counter to delay the teleportation of an enderman towards the currently attacked target */
+    private int teleportDelay;
+
+    private Entity lastEntityToAttack;
 
     public EnderslimeMobEntity(World par1World)
     {
@@ -33,8 +55,15 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
         int i = 1 << this.rand.nextInt(3);
         this.setSlimeSize(i);
 
+        // Defines if fire affects this mob.
+        this.isImmuneToFire = true;
+
         // The below means if possible, it wont walk into water
         this.getNavigator().setAvoidsWater(true);
+
+        // Swimming should ALWAYS be first. Otherwise if your mob falls in
+        // water, but it's running away from you or something it'll drown.
+        this.tasks.addTask(0, new EntityAISwimming(this));
     }
 
     protected void alterSquishAmount()
@@ -46,7 +75,55 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
     protected void applyEntityAttributes()
     {
         super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setAttribute(15.0D);
+
+        // Set mob total amount of health (hearts).
+        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setAttribute(24.0D);
+    }
+
+    /** Called when the entity is attacked. */
+    @Override
+    public boolean attackEntityFrom(DamageSource par1DamageSource, float par2)
+    {
+        if (this.isEntityInvulnerable())
+        {
+            return false;
+        }
+        else
+        {
+            if (par1DamageSource instanceof EntityDamageSourceIndirect)
+            {
+                for (int i = 0; i < 64; ++i)
+                {
+                    if (this.teleportRandomly())
+                    {
+                        return true;
+                    }
+                }
+
+                return super.attackEntityFrom(par1DamageSource, par2);
+            }
+            else
+            {
+                if (this.worldObj.rand.nextBoolean())
+                {
+                    for (int i = 0; i < 64; ++i)
+                    {
+                        if (this.teleportRandomly())
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                return super.attackEntityFrom(par1DamageSource, par2);
+            }
+        }
+    }
+
+    @Override
+    public boolean canAttackClass(Class par1Class)
+    {
+        return EnderslimeMobEntity.class != par1Class && EntityGhast.class != par1Class;
     }
 
     /** Indicates weather the slime is able to damage the player (based upon the slime's size) */
@@ -55,9 +132,39 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
         return this.getSlimeSize() > 1;
     }
 
+    @Override
+    protected void collideWithEntity(Entity par1Entity)
+    {
+        if (par1Entity instanceof IMob && !(par1Entity instanceof EnderslimeMobEntity) && this.getRNG().nextInt(20) == 0)
+        {
+            this.setAttackTarget((EntityLiving) par1Entity);
+        }
+
+        super.collideWithEntity(par1Entity);
+    }
+
+    // This is required regardless of if your animal can breed or not. Set to
+    // null if it can't breed - I wont cover breeding here.
+    public EntityAgeable createChild(EntityAgeable var1)
+    {
+        return null;
+    }
+
     protected EnderslimeMobEntity createInstance()
     {
         return new EnderslimeMobEntity(this.worldObj);
+    }
+
+    /** Drop 0-2 items of this living's type. @param par1 - Whether this entity has recently been hit by a player. @param par2 - Level of Looting used to kill this mob. */
+    @Override
+    protected void dropFewItems(boolean par1, int par2)
+    {
+        super.dropFewItems(par1, par2);
+
+        if (par1 && (this.rand.nextInt(3) == 0 || this.rand.nextInt(1 + par2) > 0))
+        {
+            this.dropItem(MadComponents.COMPONENT_ENDERSLIME.itemID, 1);
+        }
     }
 
     @Override
@@ -65,6 +172,8 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
     {
         super.entityInit();
         this.dataWatcher.addObject(16, new Byte((byte) 1));
+        this.dataWatcher.addObject(17, new Byte((byte) 0));
+        this.dataWatcher.addObject(18, new Byte((byte) 0));
     }
 
     /** Gets the amount of damage dealt to the player when "attacked" by the slime. */
@@ -123,7 +232,7 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
     @Override
     protected String getHurtSound()
     {
-        return "mob.slime." + (this.getSlimeSize() > 1 ? "big" : "small");
+        return "mob.endermen.hit";
     }
 
     /** Gets the amount of time the slime needs to wait between jumps. */
@@ -154,7 +263,7 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
     @Override
     protected float getSoundVolume()
     {
-        return 0.4F * this.getSlimeSize();
+        return 0.3F * this.getSlimeSize();
     }
 
     /** The speed it takes to move the entityliving's rotationPitch through the faceEntity method. This is only currently use in wolves. */
@@ -162,6 +271,30 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
     public int getVerticalFaceSpeed()
     {
         return 0;
+    }
+
+    /** Returns true if the WatchableObject (Byte) is 0x01 otherwise returns false. The WatchableObject is updated using setBesideClimableBlock. */
+    public boolean isBesideClimbableBlock()
+    {
+        return false;
+    }
+
+    /** returns true if this entity is by a ladder, false otherwise */
+    @Override
+    public boolean isOnLadder()
+    {
+        return this.isBesideClimbableBlock();
+    }
+
+    @Override
+    public boolean isPotionApplicable(PotionEffect par1PotionEffect)
+    {
+        return false;
+    }
+
+    public boolean isScreaming()
+    {
+        return this.dataWatcher.getWatchableObjectByte(18) > 0;
     }
 
     /** Returns true if the slime makes a sound when it jumps (based upon the slime's size) */
@@ -186,9 +319,27 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
 
             if (this.canEntityBeSeen(par1EntityPlayer) && this.getDistanceSqToEntity(par1EntityPlayer) < 0.6D * i * 0.6D * i && par1EntityPlayer.attackEntityFrom(DamageSource.causeMobDamage(this), this.getAttackStrength()))
             {
-                this.playSound("mob.attack", 1.0F, (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);
+                this.playSound("mob.endermen.scream", 1.0F, (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);
             }
         }
+    }
+
+    /** Called frequently so the entity can update its state every tick as required. For example, zombies and skeletons use this to react to sunlight and start to burn. */
+    @Override
+    public void onLivingUpdate()
+    {
+        // this.lastEntityToAttack = this.entityToAttack;
+        int i;
+
+        for (i = 0; i < 2; ++i)
+        {
+            this.worldObj.spawnParticle("portal", this.posX + (this.rand.nextDouble() - 0.5D) * this.width, this.posY + this.rand.nextDouble() * this.height - 0.25D, this.posZ + (this.rand.nextDouble() - 0.5D) * this.width,
+                    (this.rand.nextDouble() - 0.5D) * 2.0D, -this.rand.nextDouble(), (this.rand.nextDouble() - 0.5D) * 2.0D);
+        }
+
+        this.isJumping = false;
+
+        super.onLivingUpdate();
     }
 
     /** Called to update the entity's position/logic. */
@@ -248,6 +399,13 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
         this.setSlimeSize(par1NBTTagCompound.getInteger("Size") + 1);
     }
 
+    // Sets the active target the Task system uses for tracking.
+    @Override
+    public void setAttackTarget(EntityLivingBase par1EntityLivingBase)
+    {
+        super.setAttackTarget(par1EntityLivingBase);
+    }
+
     /** Will get destroyed next tick. */
     @Override
     public void setDead()
@@ -263,7 +421,7 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
                 float f = (k % 2 - 0.5F) * i / 4.0F;
                 float f1 = (k / 2 - 0.5F) * i / 4.0F;
                 EnderslimeMobEntity entityslime = this.createInstance();
-                this.entityDropItem(new ItemStack(Item.dyePowder, 1, 0), 0.0F);
+                this.entityDropItem(new ItemStack(MadComponents.COMPONENT_ENDERSLIME), 0.0F);
                 entityslime.setSlimeSize(i / 2);
                 entityslime.setLocationAndAngles(this.posX + f, this.posY + 0.5D, this.posZ + f1, this.rand.nextFloat() * 360.0F, 0.0F);
                 this.worldObj.spawnEntityInWorld(entityslime);
@@ -281,6 +439,122 @@ public class EnderslimeMobEntity extends EntityLiving implements IMob
         this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setAttribute(par1 * par1);
         this.setHealth(this.getMaxHealth());
         this.experienceValue = par1;
+    }
+
+    /** Checks to see if this enderman should be attacking this player */
+    private boolean shouldAttackPlayer(EntityPlayer par1EntityPlayer)
+    {
+        ItemStack itemstack = par1EntityPlayer.inventory.armorInventory[3];
+        Vec3 vec3 = par1EntityPlayer.getLook(1.0F).normalize();
+        Vec3 vec31 = this.worldObj.getWorldVec3Pool().getVecFromPool(this.posX - par1EntityPlayer.posX, this.boundingBox.minY + this.height / 2.0F - (par1EntityPlayer.posY + par1EntityPlayer.getEyeHeight()), this.posZ - par1EntityPlayer.posZ);
+        double d0 = vec31.lengthVector();
+        vec31 = vec31.normalize();
+        double d1 = vec3.dotProduct(vec31);
+        return d1 > 1.0D - 0.025D / d0 ? par1EntityPlayer.canEntityBeSeen(this) : false;
+    }
+
+    /** Teleport the enderman to a random nearby position */
+    protected boolean teleportRandomly()
+    {
+        double d0 = this.posX + (this.rand.nextDouble() - 0.5D) * 64.0D;
+        double d1 = this.posY + (this.rand.nextInt(64) - 32);
+        double d2 = this.posZ + (this.rand.nextDouble() - 0.5D) * 64.0D;
+        return this.teleportTo(d0, d1, d2);
+    }
+
+    /** Teleport the enderman */
+    protected boolean teleportTo(double par1, double par3, double par5)
+    {
+        if (!MadConfig.ABOMINATION_TELEPORTS)
+        {
+            return false;
+        }
+
+        EnderTeleportEvent event = new EnderTeleportEvent(this, par1, par3, par5, 0);
+        if (MinecraftForge.EVENT_BUS.post(event))
+        {
+            return false;
+        }
+
+        double d3 = this.posX;
+        double d4 = this.posY;
+        double d5 = this.posZ;
+        this.posX = event.targetX;
+        this.posY = event.targetY;
+        this.posZ = event.targetZ;
+        boolean flag = false;
+        int i = MathHelper.floor_double(this.posX);
+        int j = MathHelper.floor_double(this.posY);
+        int k = MathHelper.floor_double(this.posZ);
+        int l;
+
+        if (this.worldObj.blockExists(i, j, k))
+        {
+            boolean flag1 = false;
+
+            while (!flag1 && j > 0)
+            {
+                l = this.worldObj.getBlockId(i, j - 1, k);
+
+                if (l != 0 && Block.blocksList[l].blockMaterial.blocksMovement())
+                {
+                    flag1 = true;
+                }
+                else
+                {
+                    --this.posY;
+                    --j;
+                }
+            }
+
+            if (flag1)
+            {
+                this.setPosition(this.posX, this.posY, this.posZ);
+
+                if (this.worldObj.getCollidingBoundingBoxes(this, this.boundingBox).isEmpty() && !this.worldObj.isAnyLiquid(this.boundingBox))
+                {
+                    flag = true;
+                }
+            }
+        }
+
+        if (!flag)
+        {
+            this.setPosition(d3, d4, d5);
+            return false;
+        }
+        else
+        {
+            short short1 = 128;
+
+            for (l = 0; l < short1; ++l)
+            {
+                double d6 = l / (short1 - 1.0D);
+                float f = (this.rand.nextFloat() - 0.5F) * 0.2F;
+                float f1 = (this.rand.nextFloat() - 0.5F) * 0.2F;
+                float f2 = (this.rand.nextFloat() - 0.5F) * 0.2F;
+                double d7 = d3 + (this.posX - d3) * d6 + (this.rand.nextDouble() - 0.5D) * this.width * 2.0D;
+                double d8 = d4 + (this.posY - d4) * d6 + this.rand.nextDouble() * this.height;
+                double d9 = d5 + (this.posZ - d5) * d6 + (this.rand.nextDouble() - 0.5D) * this.width * 2.0D;
+                this.worldObj.spawnParticle("portal", d7, d8, d9, f, f1, f2);
+            }
+
+            this.worldObj.playSoundEffect(d3, d4, d5, "mob.endermen.portal", 1.0F, 1.0F);
+            this.playSound("mob.endermen.portal", 1.0F, 1.0F);
+            return true;
+        }
+    }
+
+    /** Teleport the enderman to another entity */
+    protected boolean teleportToEntity(Entity par1Entity)
+    {
+        Vec3 vec3 = this.worldObj.getWorldVec3Pool().getVecFromPool(this.posX - par1Entity.posX, this.boundingBox.minY + this.height / 2.0F - par1Entity.posY + par1Entity.getEyeHeight(), this.posZ - par1Entity.posZ);
+        vec3 = vec3.normalize();
+        double d0 = 16.0D;
+        double d1 = this.posX + (this.rand.nextDouble() - 0.5D) * 8.0D - vec3.xCoord * d0;
+        double d2 = this.posY + (this.rand.nextInt(16) - 8) - vec3.yCoord * d0;
+        double d3 = this.posZ + (this.rand.nextDouble() - 0.5D) * 8.0D - vec3.zCoord * d0;
+        return this.teleportTo(d1, d2, d3);
     }
 
     @Override
