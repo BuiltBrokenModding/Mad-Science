41d11d292d083900653e50aa67d72f271f396f9b
Maxwolf Goodliffe <devs@madsciencemod.com>
2014-10-03 03:16:05 -0700

    Removed factory API from mad science so it is only logic classes, json, and assets.

--- src/main/java/madapi/sound/MadSoundPlaybackTypeEnum.java
+++
@@ -1,8 +0,0 @@
-package madapi.sound;
-
-public enum MadSoundPlaybackTypeEnum
-{
-    LOOP,       // NO_UCD (unused code)
-    PLAY,       // NO_UCD (unused code)
-    RANDOM;     // NO_UCD (unused code)
-}
