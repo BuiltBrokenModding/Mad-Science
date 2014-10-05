86605404b4fc7ac4a4277d83c1b6e16e0f1c7b33
Maxwolf Goodliffe <devs@madsciencemod.com>
2014-10-05 00:49:58 -0700

    Testing auto generated mod meta class from editor.

--- src/main/java/madapi/MadModMetadata.java
+++ src/main/java/madapi/MadModMetadata.java
@@ -1,34 +1,26 @@
 package madapi;
 
+/** Required class for Designer support - do not modify the contents of this class with the code editor. */
+
 public class MadModMetadata
 {
-    // Identification.
-    public static final String ID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String CHANNEL_NAME = "examplemod";
-    
-    // Metadata.
-    public static final String DESCRIPTION = "Example description for Mad API!";
-    public static final String HOME_URL = "http://mygreatmod.com/";
-    
-    public static final String LOGO_PATH = "assets/examplemod/logo.png";
-    public static final String CREDITS = "Thanks to Forge team for being awesome!";
-    public static final String[] AUTHORS = {"Example Guy"};
+    public static final String ID = "madscience";
+    public static final String NAME = "Mad Science";
+    public static final String CHANNEL_NAME = "madscience";
+    public static final String DESCRIPTION = "Adds machines, items and mobs to create your own laboratory! Remember kids, science has no limits.. no bounds..";
+    public static final String HOME_URL = "http://madsciencemod.com/";
+    public static final String LOGO_PATH = "assets/madscience/logo.png";
+    public static final String CREDITS = "Thanks to Prowler for the awesome assets!";
+    public static final String[] AUTHORS = {"Maxwolf Goodliffe", "Fox Diller"};
     public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String MINECRAFT_VERSION = "[1.6.4,)";
     public static final String DEPENDENCIES = "required-after:Forge@[9.11.1.953,);after:BuildCraft|Energy;after:factorization;after:IC2;after:Railcraft;after:ThermalExpansion";
-    
-    // Proxy Classes Namespace.
     public static final String CLIENT_PROXY = "madapi.proxy.ClientProxy";
     public static final String SERVER_PROXY = "madapi.proxy.CommonProxy";
-    
-    // Full version string for internal reference by mod.
-    public static final String VMAJOR = "@MAJOR@";
-    public static final String VMINOR = "@MINOR@";
-    public static final String VREVISION = "@REVIS@";
-    public static final String VBUILD = "@BUILD@";
+    public static final String VMAJOR = "1";
+    public static final String VMINOR = "0";
+    public static final String VREVISION = "0";
+    public static final String VBUILD = "165";
     public static final String VERSION_FULL = VMAJOR + "." + VMINOR + "." + VREVISION + "." + VBUILD;
-
-    /** Update checker. */
-    public static final String UPDATE_URL = "http://mygreatmod.com/latest";
+    public static final String UPDATE_URL = "http://madsciencemod.com:8080/job/Mad%20Science/Release%20Version/api/xml?xpath=/freeStyleBuild/number";
 }
